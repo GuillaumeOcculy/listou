b73148404b5fd3059e7038c4f1a3773af2bebc6f
Guillaume <occuly.guillaume@gmail.com>
2022-10-10 23:03:24 +0200

    Add french locales

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module Listou
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.time_zone = "Europe/Paris"
   end
 end
